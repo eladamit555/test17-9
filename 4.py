77d9fef3887fd7850280cd4234a064fa572ccb4d
eladamit555 <eladamit555@gmail.com>
2026-09-17 19:47:16 +0300

    .

--- 4.py
+++ 4.py
@@ -1,6 +1,17 @@
+other_string = set()
+its_right = False
 while True:
-    word = input('Enter a word: ')
+    word = input('enter a string: ')
     if word == 'quit':
         break
-    else:
-        
+    if word[::-1] in other_string:
+        its_right = True
+        break
+    other_string.add(word)
+if its_right:
+    print('reversed match found')
+else:
+    print('no reversed match found')
+
+
+
